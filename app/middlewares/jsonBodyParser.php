5edfb7bcea422fb8fe582caf9336d9257b5b8f6b
Abhinav R <abhinav7r@gmail.com>
2022-10-02 01:47:30 +0530

    bodyparsermiddleware added

---
+++ app/middlewares/jsonBodyParser.php
@@ -0,0 +1,18 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+$jsonBodyParserMiddleware = function(Request $request, RequestHandler $handler) {
+    $contentType = $request->getHeaderLine('Content-Type');
+
+        if (strstr($contentType, 'application/json')) {
+            $contents = json_decode(file_get_contents('php://input'), true);
+            if (json_last_error() === JSON_ERROR_NONE) {
+                $request = $request->withParsedBody($contents);
+            }
+        }
+
+        return $handler->handle($request);
+};
--- app/routes/api.php
+++ app/routes/api.php
@@ -5,18 +5,24 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Factory\AppFactory;
 
+include __DIR__ . '/../middlewares/jsonBodyParser.php';
+
 $app->get('/', function(Request $request, Response $response) {
     $response_str = json_encode(['message' => 'Welcome to our Cricket API']);
     $response->getBody()->write($response_str);
     return $response->withHeader('Content-Type', 'application/json');
 });
 
+/*
+ Route to get all players
+*/
+
 $app->get('/players/', function(Request $request, Response $response) {
     
     $queryBuilder = $this->get('DB')->getQueryBuilder();
 
     $queryBuilder
-        ->select('Id', 'Name', 'Team', 'MainRole')
+        ->select('Id', 'Name', 'Team', 'Category')
         ->from('Players')
     ;
     
@@ -28,12 +34,16 @@ $app->get('/players/', function(Request $request, Response $response) {
 
 });
 
+/*
+ Route to get a single player
+*/
+
 $app->get('/player/{id}', function(Request $request, Response $response, array $args) {
     
     $queryBuilder = $this->get('DB')->getQueryBuilder();
 
     $queryBuilder
-        ->select('Id', 'Name', 'Team', 'MainRole')
+        ->select('Id', 'Name', 'Team', 'Category')
         ->from('Players')
         ->where('Id = ?')
         ->setParameter(1, $args['id'])
@@ -46,3 +56,13 @@ $app->get('/player/{id}', function(Request $request, Response $response, array $
             ->withHeader('content-type', 'application/json');
 
 });
+
+/*
+ Route to add a new player
+*/
+
+$app->post('/player/add', function(Request $request, Response $response) {
+    $parsedBody = $request->getParsedBody();
+    $response->getBody()->write(json_encode($parsedBody));
+    return $response->withHeader('content-type', 'application/json');
+})->add($jsonBodyParserMiddleware);
