ccf8c2e8223a4e3f386475a0f764bd67cdba1f4a
Abhinav R <abhinav7r@gmail.com>
2022-11-22 17:24:45 +0530

    db data added

---
+++ Players.sql
@@ -0,0 +1,15 @@
+INSERT INTO slimphp.Players (Name,Team,Category) VALUES
+	 ('Sachin Tendulkar','India','Batsman'),
+	 ('Brian Charles Lara','Windies','Batsman'),
+	 ('Shaun Pollock','South Africa','All-Rounder'),
+	 ('Lasith Malinga','Sri Lanka','Fast Bowler'),
+	 ('Shane Warne','Australia','Spin Bowler'),
+	 ('Virat Kohli','India','Batsman'),
+	 ('Curtly Ambrose','Windies','Fast Bowler'),
+	 ('Curtly Ambrose','Windies','Fast Bowler'),
+	 ('Curtly Ambrose','Windies','Fast Bowler'),
+	 ('Curtly Ambrose','Windies','Fast Bowler');
+INSERT INTO slimphp.Players (Name,Team,Category) VALUES
+	 ('Curtly Ambrose','Windies','Fast Bowler'),
+	 ('Curtly Ambrose','Windies','Fast Bowler'),
+	 ('Curtly Ambrose','Windies','Fast Bowler');
---
+++ Users.sql
@@ -0,0 +1,2 @@
+INSERT INTO slimphp.Users (ApiKey,UserName) VALUES
+	 ('$2y$10$24Sndk.UbSV873dDED6LeevKC4u0v3yb7VpQRGGo/r5oOqkeZVviG','abhinav');
